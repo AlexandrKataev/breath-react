88fb96e05c4ec56db857e8ba962b9fa53e68d9f5
AlexandrKataev <dkeyman@mail.ru>
2022-10-14 23:15:11 +0300

    adaptive

--- src/components/Breath/Breath.tsx
+++ src/components/Breath/Breath.tsx
@@ -5,10 +5,8 @@ import breathImage from '../../assets/images/breath.png';
 
 const Breath: React.FC = () => {
   return (
-    <div>
-      <div className={s.body}>
-        <img className={s.breath} src={breathImage} alt="image not found"></img>
-      </div>
+    <div className={s.body}>
+      <img className={s.breath} src={breathImage} alt="image not found"></img>
     </div>
   );
 };
