b804997b074bd06a3d46240ad0acbdf5ef5d49c4
Alexander Kataev <sanekjackie@gmail.com>
2025-05-09 20:34:19 +0300

    рефакторинг стилей

--- src/entities/Lungs/Lungs.tsx
+++ src/entities/Lungs/Lungs.tsx
@@ -1,14 +1,14 @@
-import { FC } from 'react';
-import cx from 'classnames';
-import s from './Lungs.module.scss';
+import cx from "classnames";
+import { FC } from "react";
+import s from "./Lungs.module.scss";
 
-import { useAppSelector } from 'app/store/hooks';
+import { useAppSelector } from "app/store/hooks";
 import {
   selectBreathIsVdoh,
   selectBreathIsVidoh,
   selectBreathIsZad,
   selectBreathStarted,
-} from 'widgets/Breath/model/selectors';
+} from "widgets/Breath/model/selectors";
 
 export const Lungs: FC = () => {
   const started = useAppSelector(selectBreathStarted);
@@ -17,16 +17,14 @@ export const Lungs: FC = () => {
   const isZad = useAppSelector(selectBreathIsZad);
 
   return (
-    <div className={s.body}>
-      <div id={s.myContainer}>
-        <div
-          id={s.myContainer2}
-          className={cx(
-            { [s.animVdoh]: isVdoh && started },
-            { [s.animVidoh]: isVidoh && started },
-            { [s.animZad]: isZad && started },
-          )}></div>
-      </div>
+    <div className={s.lungs}>
+      <div
+        className={cx(
+          s.lungs2,
+          { [s.animVdoh]: isVdoh && started },
+          { [s.animVidoh]: isVidoh && started },
+          { [s.animZad]: isZad && started }
+        )}></div>
     </div>
   );
 };
